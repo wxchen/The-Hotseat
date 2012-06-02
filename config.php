cc8592724f047bc55b07cec348fddc81ca3fb565
Farid Wardan <farid@wardan.com>
2012-06-02 15:16:17 +1000

    Updated config file

--- config.php
+++ config.php
@@ -1,5 +1,15 @@
 <?php
 define('DB_HOST','23.23.245.140');
-define('DB_USERNAME','dbuser');
+define('DB_USER','dbuser');
 define('DB_PASSWORD','b055');
 define('DB_DATABASE','govhack_prod');
+
+try
+{
+	$pdo = new PDO('mysql:dbname='.DB_DATABASE.';host='.DB_HOST,DB_USER,DB_PASSWORD);
+}
+catch (PDOException $e)
+{
+	echo 'Connection failed: ' . $e->getMessage();
+	die;
+}
